b48a4d79072b8897a1797c483df8cc2792f7eaab
t-kalden <kalden.t@outlook.com>
2021-11-30 08:47:41 -0500

    Built out HTML to set up barebones markups, and added call functionality to request data from tvmaze api and display it to the page.

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const searchTerm = form.elements.query.value;
+
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium
+    document.body.append(img);
+})
