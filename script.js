967cb59698f96e4d19855b993922c7689f0303b9
t-kalden <kalden.t@outlook.com>
2021-11-30 21:47:15 -0500

    Added Show name and genre to be shown when searched

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const form = document.querySelector("#searchForm");
+const showBox = document.querySelector('#showBox');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-
+    showBox.innerHTML = '';
     const searchTerm = form.elements.query.value;
-
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    const resData = res.data[0];
+    console.log(resData)
     const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium
-    document.body.append(img);
+    const name = document.createElement('H3');
+    const genre = document.createElement('SPAN');
+
+    img.src = resData.show.image.medium;
+    name.innerHTML = resData.show.name;
+    genre.innerHTML = resData.show.seasons;
+
+    showBox.append(img);
+    showBox.append(name);
+    showBox.append(genre);
 })
